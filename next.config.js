1bb149ea79639ca24061c6ce83e2640f75aef6ae
amigolargo <tomlongmate@gmail.com>
2023-02-02 12:03:36 +0100

    pass build time env vars through nextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
+  },
 }
 
 module.exports = nextConfig
